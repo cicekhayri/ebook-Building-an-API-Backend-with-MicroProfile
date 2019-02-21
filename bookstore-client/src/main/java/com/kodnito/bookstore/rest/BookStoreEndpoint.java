cd3e8de039574f87d6a1d451935ae313ebd8703e
Hayri Cicek <hayri@kodnito.com>
2019-02-21 10:12:00 +0100

    updated MP config page

--- bookstore-client/src/main/java/com/kodnito/bookstore/rest/BookStoreEndpoint.java
+++ bookstore-client/src/main/java/com/kodnito/bookstore/rest/BookStoreEndpoint.java
@@ -9,17 +9,22 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
 import com.kodnito.bookstore.service.BookStoreService;
+import javax.inject.Inject;
 import javax.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 @Path("/books")
 public class BookStoreEndpoint {
 
+    @Inject
+    @ConfigProperty(name = "book-store.base-url")
+    private String bookStoreBaseUrl;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response books() throws MalformedURLException {
-        URL url = new URL("http://localhost:8080/restapi");
+        URL url = new URL(bookStoreBaseUrl);
         BookStoreService bookStoreService
                 = RestClientBuilder.newBuilder()
                         .baseUrl(url)

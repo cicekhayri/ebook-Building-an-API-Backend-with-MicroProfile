5556169a21a866ceb4798bf7d05da244c01aca53
Hayri Cicek <hayri@kodnito.com>
2019-02-25 22:09:01 +0100

    Inject the BookStoreService and updated the microprofile-rest-client page

--- bookstore-client/src/main/java/com/kodnito/bookstore/rest/BookStoreEndpoint.java
+++ bookstore-client/src/main/java/com/kodnito/bookstore/rest/BookStoreEndpoint.java
@@ -1,35 +1,27 @@
 package com.kodnito.bookstore.rest;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import org.eclipse.microprofile.rest.client.RestClientBuilder;
 import com.kodnito.bookstore.service.BookStoreService;
 import javax.inject.Inject;
 import javax.ws.rs.core.Response;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @ApplicationScoped
 @Path("/books")
 public class BookStoreEndpoint {
 
     @Inject
-    @ConfigProperty(name = "book-store.base-url")
-    private String bookStoreBaseUrl;
+    @RestClient
+    private BookStoreService bookStoreService;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response books() throws MalformedURLException {
-        URL url = new URL(bookStoreBaseUrl);
-        BookStoreService bookStoreService
-                = RestClientBuilder.newBuilder()
-                        .baseUrl(url)
-                        .build(BookStoreService.class);
-
         return Response.ok(bookStoreService.getAll()).build();
     }
 }

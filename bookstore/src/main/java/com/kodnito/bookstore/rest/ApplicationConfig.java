2d5e57ce0f4d5ed8af51c5183538d6241b3615a7
Hayri Cicek <hayri@kodnito.com>
2019-10-26 21:48:55 +0200

    ApplicationConfig class added

---
+++ bookstore/src/main/java/com/kodnito/bookstore/rest/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.kodnito.bookstore.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class ApplicationConfig extends Application {
+    
+}
